77d328e1c234af868ebaab3abaacb7f5cbfb1080
Daniele Marino <me@danielemarino.net>
2012-10-24 23:30:42 +0200

    First commit. No inline code documentation.

---
+++ CsvReader.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+namespace ZPL.Tools
+{
+
+  /// <summary>
+  /// A data-reader style interface for reading CSV files.
+  /// </summary>
+  public class CsvReader : IDisposable
+  {
+
+    #region Private variables
+
+    private readonly Stream _stream;
+    private readonly StreamReader _reader;
+    private char _separator = ',';
+
+    #endregion
+
+    public char Separator
+    {
+      get
+      {
+        return _separator;
+      }
+      set
+      {
+        _separator = value;
+      }
+    }
+
+    /// <summary>
+    /// Create a new reader for the given stream.
+    /// </summary>
+    /// <param name="s">The stream to read the CSV from.</param>
+    public CsvReader(Stream s) : this(s, null) { }
+
+    /// <summary>
+    /// Create a new reader for the given stream and encoding.
+    /// </summary>
+    /// <param name="s">The stream to read the CSV from.</param>
+    /// <param name="enc">The encoding used.</param>
+    public CsvReader(Stream s, Encoding enc)
+    {
+
+      _stream = s;
+      if (!s.CanRead)
+      {
+        throw new CsvReaderException("Could not read the given CSV stream!");
+      }
+      _reader = (enc != null) ? new StreamReader(s, enc) : new StreamReader(s);
+    }
+
+    /// <summary>
+    /// Creates a new reader for the given text file path.
+    /// </summary>
+    /// <param name="filename">The name of the file to be read.</param>
+    public CsvReader(string filename) : this(filename, null) { }
+
+    /// <summary>
+    /// Creates a new reader for the given text file path and encoding.
+    /// </summary>
+    /// <param name="filename">The name of the file to be read.</param>
+    /// <param name="enc">The encoding used.</param>
+    public CsvReader(string filename, Encoding enc)
+      : this(new FileStream(filename, FileMode.Open), enc) { }
+
+    /// <summary>
+    /// Returns the fields for the next row of CSV data (or null if at eof)
+    /// </summary>
+    /// <returns>A string array of fields or null if at the end of file.</returns>
+    public string[] GetCsvLine()
+    {
+
+      string data = _reader.ReadLine();
+
+      // REPLACE REPLACE REPLACE
+      //data = data.Replace("\"", "");
+
+      if (data == null) return null;
+      if (data.Length == 0) return new string[0];
+
+      var result = new ArrayList();
+
+      ParseCsvFields(result, data);
+
+      return (string[])result.ToArray(typeof(string));
+    }
+
+    // Parses the CSV fields and pushes the fields into the result arraylist
+    private void ParseCsvFields(ArrayList result, string data)
+    {
+
+      int pos = -1;
+      while (pos < data.Length)
+        result.Add(ParseCsvField(data, ref pos));
+    }
+
+    // Parses the field at the given position of the data, modified pos to match
+    // the first unparsed position and returns the parsed field
+    private string ParseCsvField(string data, ref int startSeparatorPosition)
+    {
+
+      if (startSeparatorPosition == data.Length - 1)
+      {
+        startSeparatorPosition++;
+        // The last field is empty
+        return "";
+      }
+
+      var fromPos = startSeparatorPosition + 1;
+
+      // Determine if this is a quoted field
+      if (data[fromPos] == '"')
+      {
+        // If we're at the end of the string, let's consider this a field that
+        // only contains the quote
+        if (fromPos == data.Length - 1)
+        {
+//          fromPos++;
+          return "\"";
+        }
+
+        // Otherwise, return a string of appropriate length with double quotes collapsed
+        // FSQ returns data.Length if no single quote was found
+        var nextSingleQuote = FindSingleQuote(data, fromPos + 1);
+        startSeparatorPosition = nextSingleQuote + 1;
+        return data.Substring(fromPos + 1, nextSingleQuote - fromPos - 1).Replace("\"\"", "\"");
+      }
+
+      // The field ends in the next comma or EOL
+      var nextComma = data.IndexOf(';', fromPos);
+      if (nextComma == -1)
+      {
+        startSeparatorPosition = data.Length;
+        return data.Substring(fromPos);
+      }
+      startSeparatorPosition = nextComma;
+      return data.Substring(fromPos, nextComma - fromPos);
+    }
+
+    // Returns the index of the next single quote mark in the string 
+    // (starting from startFrom)
+    private static int FindSingleQuote(string data, int startFrom)
+    {
+
+      int i = startFrom - 1;
+      while (++i < data.Length)
+        if (data[i] == '"')
+        {
+          // If this is a double quote, bypass the chars
+          if (i < data.Length - 1 && data[i + 1] == '"')
+          {
+            i++;
+            continue;
+          }
+          return i;
+        }
+      // If no quote found, return the end value of i (data.Length)
+      return i;
+    }
+
+    /// <summary>
+    /// Disposes the CSVReader. The underlying stream is closed.
+    /// </summary>
+    public void Dispose()
+    {
+      // Closing the reader closes the underlying stream, too
+      if (_reader != null) _reader.Close();
+      else if (_stream != null)
+        _stream.Close(); // In case we failed before the reader was constructed
+      GC.SuppressFinalize(this);
+    }
+  }
+
+
+  /// <summary>
+  /// Exception class for CSVReader exceptions.
+  /// </summary>
+  public class CsvReaderException : ApplicationException
+  {
+
+    /// <summary>
+    /// Constructs a new exception object with the given message.
+    /// </summary>
+    /// <param name="message">The exception message.</param>
+    public CsvReaderException(string message) : base(message) { }
+  }
+
+}
---
+++ ZplFile.cs
@@ -0,0 +1,164 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZPL.Tools
+{
+  public class ZplFile
+  {
+    #region Private
+
+    private const string CmdDownloadGraphicImage = "~DG";
+    private static readonly string[] CmdChar = new[] { "^", "~" };
+
+    #endregion
+
+    #region Properties
+
+    public long Length 
+    {
+      get { return Text.Length; }
+    }
+
+    public string Text { get; private set; }
+
+    #endregion  
+
+    #region Constructors
+
+    public ZplFile(string myText)
+    {
+      Text = myText;
+    }
+
+    public ZplFile()
+    {
+      Text = "";
+    }
+
+    #endregion
+
+    #region Methods
+
+    public int ImageStrip(int imageIndex)
+    {
+      if (imageIndex < 0 || imageIndex >= (Text.Length-CmdDownloadGraphicImage.Length)) return 2;
+      if (Text.Substring(imageIndex, CmdDownloadGraphicImage.Length) != CmdDownloadGraphicImage) return 1;
+
+      var nextCommandIndex = Text.IndexOf('^', imageIndex);
+
+      if (nextCommandIndex > 0)
+        Text = Text.Substring(0, imageIndex - 1) + "\n" + Text.Substring(nextCommandIndex);
+      else
+        Text = Text.Substring(0, imageIndex - 1);
+      
+      return 0;
+    }
+
+    public string ImageExtractAndStrip(int imageIndex)
+    {
+      if (imageIndex < 0 || imageIndex >= (Text.Length - CmdDownloadGraphicImage.Length)) return "";
+      var imageCode = ImageExtract(imageIndex);
+      ImageStrip(imageIndex);
+
+      return imageCode;
+    }
+
+    public string ImageExtract(int imageIndex)
+    {
+      if (imageIndex < 0 || imageIndex >= (Text.Length - CmdDownloadGraphicImage.Length)) return "";
+      if (Text.Substring(imageIndex, CmdDownloadGraphicImage.Length) != CmdDownloadGraphicImage) return "";
+        
+      var nextCommandIndex = Text.IndexOf('^', imageIndex);
+      return nextCommandIndex>0 ? Text.Substring(imageIndex, nextCommandIndex - imageIndex) : Text.Substring(imageIndex);
+    }
+
+    public int ImageIndexOf()
+    {
+      return Text.IndexOf(CmdDownloadGraphicImage);
+    }
+
+    public int ImageLastIndexOf()
+    {
+      return Text.LastIndexOf(CmdDownloadGraphicImage);
+    }
+
+    public long ImagesCount()
+    {
+        return new System.Text.RegularExpressions.Regex(CmdDownloadGraphicImage).Matches(Text).Count;
+    }
+
+    public bool IsValidZpl()
+    {
+      return true;
+    }
+
+    public string CommandExtract(int commandIndex)
+    {
+      if (commandIndex < 0 || commandIndex >= (Text.Length - 1)) return "";
+      if (Text.Substring(commandIndex, 1) != "^") return "";
+
+      var nextCommandIndex = Text.IndexOf('^', commandIndex+1);
+      return nextCommandIndex > 0 ? Text.Substring(commandIndex, nextCommandIndex - commandIndex) : Text.Substring(commandIndex);
+    }
+
+    public int CommandStrip(int commandIndex)
+    {
+      if (commandIndex < 0 || commandIndex >= (Text.Length - CmdDownloadGraphicImage.Length)) return 2;
+      if (Text.Substring(commandIndex, 1) != "^") return 1;
+
+      var nextCommandIndex = Text.IndexOf('^', commandIndex+1);
+
+      if (nextCommandIndex > 0)
+        Text = Text.Substring(0, commandIndex - 1) + "\n" + Text.Substring(nextCommandIndex);
+      else
+        Text = Text.Substring(0, commandIndex - 1);
+
+      return 0;
+    }
+
+    public int CommandIndex(int charIndex)
+    {
+      if (charIndex < 0 || charIndex >= Text.Length) return -1;
+      var myIndex = new int[CmdChar.Length];
+
+      for (var i = 0; i < CmdChar.Length; i++)
+      {
+        if (Text.Substring(charIndex, 1) == CmdChar[i]) return charIndex;
+        myIndex[i] = Text.Substring(0, charIndex - 1).LastIndexOf(CmdChar[i]);
+      }
+
+      if (CmdChar.Length == 1) return myIndex[0];
+
+      var myMax = 0;
+      for (var i = 1; i < CmdChar.Length; i++)
+      {
+        myMax = Math.Max(myIndex[i], myIndex[i - 1]);
+      }
+      return myMax;
+    }
+
+    public int NextCommandIndex(int charIndex)
+    {
+      return charIndex;
+    }
+
+    public int PrevCommandIndex(int charIndex)
+    {
+      if (charIndex <= 0 || charIndex >= Text.Length) return -1;
+      var currentCommandIndex = CommandIndex(charIndex);
+      return CommandIndex(currentCommandIndex - 1);
+    }
+
+    #endregion
+  
+  }
+}
+
+// TODO: cambiare il "estrai immagine" e il "strip immagine" in "estrai comando" e il "strip comando".
+// TODO: i caratteri di inizio comando sono due: ^ e tilde
+// TODO: verificare Command, PreviousCommand e NextCommand. contando sempre tutti e due i separatori.
+
+
