c2dfa5fb3a8d21eb29bdcfbe557fed86f18002cf
Daniele Marino <me@danielemarino.net>
2012-10-26 14:45:09 +0200

    Code reduced to a single file. Inline docs still missing.

--- ZplFile.cs
+++ ZplFile.cs
@@ -2,26 +2,136 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using ZPL.Tools;
 
 namespace ZPL.Tools
 {
+  public static class ZplLanguage
+  {
+    public static Dictionary<string, string> Commands = new Dictionary<string, string>();
+
+    static ZplLanguage()
+    {
+      //ZPL II Basic
+      Commands.Add("^XA", "");
+      Commands.Add("^XZ", "");
+      Commands.Add("^LH", "");
+      Commands.Add("^LL", "");
+      Commands.Add("^ML", "");
+      Commands.Add("^LS", "");
+      Commands.Add("^P", "");
+      Commands.Add("^FW", "");
+      Commands.Add("^PO", "");
+      Commands.Add("^FO", "");
+      Commands.Add("^FT", "");
+      Commands.Add("^FP", "");
+      Commands.Add("^FD", "");
+      Commands.Add("^FS", "");
+      Commands.Add("^FH", "");
+      // Printer Configuration
+      Commands.Add("^MM", "");
+      Commands.Add("^MN", "");
+      Commands.Add("^MT", "");
+      Commands.Add("^MU", "");
+      Commands.Add("^MD", "");
+      Commands.Add("~SD", "");
+      // Text for Labels
+      Commands.Add("^CI", "");
+      Commands.Add("^CF", "");
+      Commands.Add("^A", "");
+      Commands.Add("^A0", "");
+      Commands.Add("^A@", "");
+      Commands.Add("~DB", "");
+      Commands.Add("~DU", "");
+      Commands.Add("^CW", "");
+      // Bar Codes
+      Commands.Add("^B2", "");
+      Commands.Add("^BK", "");
+      Commands.Add("^BP", "");
+      Commands.Add("^BZ", "");
+      Commands.Add("^B8", "");
+      Commands.Add("^B9", "");
+      Commands.Add("^BE", "");
+      Commands.Add("^BS", "");
+      Commands.Add("^BU", "");
+      Commands.Add("^B3", "");
+      Commands.Add("^BA", "");
+      Commands.Add("^BC", "");
+      // Two-Dimensional Bar Code
+      Commands.Add("^B7", "");
+      Commands.Add("^BD", "");
+      Commands.Add("^BX", "");
+      // Bar Codes others
+      Commands.Add("^BY", "");
+      // Graphic Instruction
+      Commands.Add("^GB", "");
+      Commands.Add("^GC", "");
+      Commands.Add("^GD", "");
+      Commands.Add("^DD", "");
+      Commands.Add("~DG", "");
+      Commands.Add("^GF", "");
+      Commands.Add("~DY", "");
+      Commands.Add("^XG", "");
+      Commands.Add("^IM", "");
+      Commands.Add("^IS", "");
+      Commands.Add("^IL", "");
+      Commands.Add("^ID", "");
+      Commands.Add("^EG", "");
+      Commands.Add("~EG", "");
+      // Advanced Techniques
+      Commands.Add("^FX", "");
+      Commands.Add("^FR", "");
+      Commands.Add("^LR", "");
+      Commands.Add("^PM", "");
+      Commands.Add("^SN", "");
+      Commands.Add("~TA", "");
+      Commands.Add("^EF", "");
+      Commands.Add("~EF", "");
+      Commands.Add("^DF", "");
+      Commands.Add("^XF", "");
+      Commands.Add("^ST", "");
+      Commands.Add("~JR", "");
+      Commands.Add("~JC", "");
+      Commands.Add("~JA", "");
+      Commands.Add("~JP", "");
+      Commands.Add("~JX", "");
+      Commands.Add("~PH", "");
+      Commands.Add("^PH", "");
+      Commands.Add("~PP", "");
+      Commands.Add("^PP", "");
+      Commands.Add("~PS", "");
+      Commands.Add("^PQ", "");
+      Commands.Add("^PR", "");
+      Commands.Add("~PR", "");
+      Commands.Add("^JM", "");
+      Commands.Add("^CC", "");
+      Commands.Add("~CC", "");
+      Commands.Add("^CD", "");
+      Commands.Add("~CD", "");
+      Commands.Add("^CT", "");
+      Commands.Add("~CT", "");
+      Commands.Add("^CM", "");
+      Commands.Add("~JD", "");
+      Commands.Add("~WC", "");
+    }
+
+    public static long Count
+    {
+      get
+      {
+        return Commands.Count;
+      }
+    }
+
+  }
+
   public class ZplFile
   {
-    #region Private
-
-    private static readonly string[] CmdChar = new[] { "^", "~" };
-    //private HashSet<string> _zplCommands = new HashSet<string>(); 
-
-    #endregion
-
     #region Properties
 
     /// <summary>
     /// 
     /// </summary>
-    public long Length 
+    public int Length 
     {
       get { return Text.Length; }
     }
@@ -77,93 +187,120 @@ namespace ZPL.Tools
       return "";
     }
 
-    public long CommandCount(string commandTag)
+    public string GetCommandParams(int commandIndex)
+    {
+      var commandTag = "";
+      for (var i = 1; i <= 3; i++)
+      {
+        if (!ZplLanguage.Commands.ContainsKey(Text.Substring(commandIndex, i))) continue;
+        commandTag= Text.Substring(commandIndex, i);
+        break;
+      }
+      return CommandExtract(commandIndex).Replace(commandTag, "");
+    }
+
+    public bool IsValidCommandTag(int charIndex)
+    {
+      try
+      {
+        for (var i = 1; i <= 3; i++)
+        {
+          if (ZplLanguage.Commands.ContainsKey(Text.Substring(charIndex, i))) return true;
+        }
+        return false;
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+    }
+
+    public int CommandCount(string commandTag)
     {
       return new System.Text.RegularExpressions.Regex(commandTag).Matches(Text).Count;
     }
 
-    public long CommandCount(string[] commandTags)
+    public int CommandCount(string[] commandTags)
     {
-      long totalCount = 0;
-      for (var i = 0; i < commandTags.Length; i++)
-      {
-        totalCount += new System.Text.RegularExpressions.Regex(commandTags[i]).Matches(Text).Count;
-      }
-      return totalCount;
+      return commandTags.Sum(t => new System.Text.RegularExpressions.Regex(t).Matches(Text).Count);
     }
 
+    public int CommandPosition(int charIndex)
+    {
+      if (charIndex < 0 || charIndex >= Text.Length) return -1; // Wrong Request
+
+      for (var i = charIndex; i >=0; i--)
+      {
+        if (IsValidCommandTag(i)) return i;
+      }
+
+      return -2; // No Command Found
+    }
+
+    public int NextCommandPosition(int charIndex)
+    {
+      return CommandIndexOf(CommandPosition(charIndex) + 1);
+    }
+    
+    public int PrevCommandPosition (int charIndex)
+    {
+      return CommandPosition(CommandPosition(charIndex) - 1);
+    }
+    
     public int CommandIndexOf(int charIndex)
     {
-      if (charIndex < 0 || charIndex >= Text.Length) return -1;
-      var myIndex = new int[CmdChar.Length];
+      if (charIndex < 0 || charIndex >= Text.Length) return -1; // Wrong Request
 
-      for (var i = 0; i < CmdChar.Length; i++)
+      for (var i = charIndex; i < Length; i++)
       {
-        if (Text.Substring(charIndex, 1) == CmdChar[i]) return charIndex;
-        myIndex[i] = Text.Substring(0, charIndex - 1).LastIndexOf(CmdChar[i]);
+        if (IsValidCommandTag(i)) return i;
       }
 
-      if (CmdChar.Length == 1) return myIndex[0];
-
-      var myMax = 0;
-      for (var i = 1; i < CmdChar.Length; i++)
-      {
-        myMax = Math.Max(myIndex[i], myIndex[i - 1]);
-      }
-      return myMax;
+      return -2; // No Command Found
     }
 
-    public int CommandIndexOf(int charIndex, string commandTag)
+    public int CommandIndexOf(string commandTag, int charIndex)
     {
-      if (charIndex < 0 || charIndex >= Text.Length) return -1;
-      var myIndex = new int[CmdChar.Length];
-
-      for (var i = 0; i < CmdChar.Length; i++)
-      {
-        if (Text.Substring(charIndex, 1) == CmdChar[i]) return charIndex;
-        myIndex[i] = Text.Substring(0, charIndex - 1).LastIndexOf(CmdChar[i]);
-      }
-
-      if (CmdChar.Length == 1) return myIndex[0];
-
-      var myMax = 0;
-      for (var i = 1; i < CmdChar.Length; i++)
-      {
-        myMax = Math.Max(myIndex[i], myIndex[i - 1]);
-      }
-      return myMax;
+      return Text.IndexOf(commandTag, charIndex);
     }
 
-    public int CommandLastIndexOf(int charIndex)
+    public int CommandIndexOf(string commandTag)
     {
-      if (charIndex <= 0 || charIndex >= Text.Length) return -1;
-      var currentCommandIndex = CommandIndexOf(charIndex);
-      return CommandIndexOf(currentCommandIndex - 1);
+      return Text.IndexOf(commandTag, 0);
     }
 
-    public int CommandLastIndexOf(int charIndex, string commandTag)
+    public int CommandLastIndexOf()
     {
-      if (charIndex <= 0 || charIndex >= Text.Length) return -1;
-      var currentCommandIndex = CommandIndexOf(charIndex);
-      return CommandIndexOf(currentCommandIndex - 1);
+      for (var i = Length; i > 0; i--)
+      {
+        if (IsValidCommandTag(i)) return i;
+      }
+
+      return -2; // No Command Found
+    }
+
+    public int CommandLastIndexOf(string commandTag)
+    {
+      return Text.LastIndexOf(commandTag);
     }
 
     public string CommandExtract(int commandIndex)
     {
-      if (commandIndex < 0 || commandIndex >= (Text.Length - 1)) return "";
-      if (!(ZplLanguage.Commands.ContainsKey(Text.Substring(commandIndex, 2)) || ZplLanguage.Commands.ContainsKey(Text.Substring(commandIndex, 3)))) return "";
+      if (commandIndex < 0 || commandIndex >= (Text.Length)) return "";
+      if (!IsValidCommandTag(commandIndex)) return "";
 
-      var nextCommandIndex = Text.IndexOf('^', commandIndex + 1);
-      return nextCommandIndex > 0 ? Text.Substring(commandIndex, nextCommandIndex - commandIndex) : Text.Substring(commandIndex);
+      var nextCommandIndex = CommandIndexOf(commandIndex + 1);
+      return nextCommandIndex > 0
+               ? Text.Substring(commandIndex, nextCommandIndex - commandIndex)
+               : Text.Substring(commandIndex);
     }
 
     public int CommandStrip(int commandIndex)
     {
-      var commandTag = GetCommandTag(commandIndex);
-      if (commandIndex < 0 || commandIndex >= (Text.Length - commandTag.Length)) return 2;
-      if (!ZplLanguage.Commands.ContainsKey(Text.Substring(commandIndex, 3))) return 1;
+      if (commandIndex < 0 || commandIndex >= Text.Length) return -1;
+      if (!IsValidCommandTag(commandIndex)) return -2;
 
-      var nextCommandIndex = Text.IndexOf('^', commandIndex + 1);
+      var nextCommandIndex = CommandIndexOf(commandIndex + 1);
 
       if (nextCommandIndex > 0)
         Text = Text.Substring(0, commandIndex - 1) + "\n" + Text.Substring(nextCommandIndex);
